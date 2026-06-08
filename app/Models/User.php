310b75ba78d32acec667fea813b6e08f5d206e3f
Greazi <mail@greazi.com>
2026-06-08 21:36:58 +0200

    update canAccessPanel method to always return true

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,6 +36,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->hasVerifiedEmail();
+        return true;
     }
 }
